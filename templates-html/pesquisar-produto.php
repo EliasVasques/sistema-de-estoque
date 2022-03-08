d39fc545b251f4fa59f6eeee2fc94a0cc433272b
EliasVasques <81265552+EliasVasques@users.noreply.github.com>
2022-03-07 21:32:57 -0300

    Pesquisa de produto arquivo add

---
+++ templates-html/pesquisar-produto.php
@@ -0,0 +1,38 @@
+
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Produto</th>
+      <th scope="col">Preço</th>
+      <th scope="col">Código</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Soda</td>
+      <td>R$2.88</td>
+      <td>1112638321</td>
+    </tr>
+    <tr>
+    <td>Soda</td>
+      <td>R$2.88</td>
+      <td>1112638321</td>
+    </tr>
+    <tr>
+      <td>Soda</td>
+      <td>R$2.88</td>
+      <td>1112638321</td>
+    </tr>
+  </tbody>
+</table>
+
+<nav aria-label="Page navigation example" class="paginacao">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
