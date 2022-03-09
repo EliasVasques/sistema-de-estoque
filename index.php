55473e85f2882d6477090929017d3c1704924d5f
EliasVasques <81265552+EliasVasques@users.noreply.github.com>
2022-03-08 21:05:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- meu css -->
+    <link rel="stylesheet" href="style/geral.css">
 
     <!-- font awesome --> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/
@@ -42,6 +43,9 @@ font-awesome/5.15.2/css/all.min.css"/>
           case 'login-tela':
             include 'templates-html/login-tela.php';
             break; 
+          case 'entradas-e-saidas':
+            include 'templates-html/entradas-e-saidas.php';
+            break;
         }
       }
       
