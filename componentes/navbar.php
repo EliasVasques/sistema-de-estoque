708994f7fe3394ee73978f1a1686ff0dac208bdb
EliasVasques <81265552+EliasVasques@users.noreply.github.com>
2022-03-11 21:37:21 -0300

    Add files via upload

---
+++ componentes/navbar.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../img/logo.jpg" alt="" class="logo">
+      Organize
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../home">Home</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Adm
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../adm/entradas-e-saidas.php">Entradas e Saídas</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Produto
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../produto/comprar.php">Comprar</a></li>
+            <li><a class="dropdown-item" href="../produto/cadastrar.php">Cadastrar</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../login">Sair</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ home/index.php
@@ -0,0 +1,40 @@
+<?php require '../componentes/head.php' ?>
+<?php require '../componentes/navbar.php' ?>
+<body>
+
+<h2>Sistema de Gerenciamento</h2>
+
+<div class="cards">
+    <a href="">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/cadastro.png" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Cadastro</h5>
+            <p class="card-text"></p>
+        </div>
+        </div>
+    </a>
+
+    <a href="../compra-e-venda">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/compra-e-venda.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Compra e Venda</h5>
+            <p class="card-text"></p>
+        </div>
+        </div>
+    </a>
+    
+    <a href="">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../img/buscar.png" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">Buscar no sistema</h5>
+            <p class="card-text"></p>
+        </div>
+        </div>
+    </a>
+    
+</div>
+
+<?php require '../componentes/footer.php' ?>
