6dc487fea4ad0efcf7e091dee655f215de8d57f6
EliasVasques <81265552+EliasVasques@users.noreply.github.com>
2022-03-11 21:36:41 -0300

    Add files via upload

---
+++ sistema-de-estoque/produto/cadastrar.php
@@ -0,0 +1,31 @@
+<?php require '../componentes/head.php' ?>
+<?php require '../componentes/navbar.php' ?>
+<body>
+
+<h2 class="titulo">Cadastrar Produto</h2>
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-12">
+      <label for="nome">Nome</label>
+      <input type="text" class="form-control" id="nome">
+    </div>
+    <div class="form-group col-md-12">
+      <label for="marca">Marca</label>
+      <select id="marca" class="form-control">
+        <option selected>-</option>
+        <option>Skol</option>
+        <option>Calsberg Group</option>
+        <option>Cuca BGI</option>
+      </select>
+    </div>
+    <div class="form-group col-md-12">
+      <label for="preco">Preço</label>
+      <input type="text" class="form-control" id="preco">
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Cadastrar</button>
+</form>
+
+<?php require '../componentes/footer.php' ?>
+
