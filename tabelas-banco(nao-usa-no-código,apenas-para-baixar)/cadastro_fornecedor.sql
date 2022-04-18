fbc5470c3045f19619b4e024489a42e07fbc2045
Eliseu <eliseuvasques09@gmail.com>
2022-04-18 16:39:09 -0300

    tabelas banco

---
+++ tabelas-banco(nao-usa-no-código,apenas-para-baixar)/cadastro_fornecedor.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18-Abr-2022 às 21:37
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cadastro_fornecedor`
+--
+
+CREATE TABLE `cadastro_fornecedor` (
+  `idFornecedor` int(10) NOT NULL,
+  `nomeFantasia` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `razaoSocial` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CNPJ` int(11) NOT NULL,
+  `endereco` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `UF` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cidade` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CEP` int(8) NOT NULL,
+  `representante` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefone` int(11) NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cadastro_fornecedor`
+--
+ALTER TABLE `cadastro_fornecedor`
+  ADD PRIMARY KEY (`idFornecedor`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cadastro_fornecedor`
+--
+ALTER TABLE `cadastro_fornecedor`
+  MODIFY `idFornecedor` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tabelas-banco(nao-usa-no-código,apenas-para-baixar)/carrinho.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 18-Abr-2022 às 21:37
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `carrinho`
+--
+
+CREATE TABLE `carrinho` (
+  `codigoBarra` int(13) NOT NULL,
+  `tipoProduto` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `marca` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `codigoFornecedor` int(11) NOT NULL,
+  `valor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD PRIMARY KEY (`codigoBarra`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
