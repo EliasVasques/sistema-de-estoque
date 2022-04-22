e3f31fff3be1e701592e771c912dce8a37ec6744
Victor Ramsés <82965282+VRamses@users.noreply.github.com>
2022-04-22 20:16:24 -0300

    Primeira tentativa - Busca cliente

--- busca-sistema/cliente.php
+++ busca-sistema/cliente.php
@@ -17,53 +17,61 @@
 
 <h2 id="titulo">Clientes</h2>
 
-<div class="input-group rounded" id="buscar-mercadoria">
-  <input type="search" class="form-control rounded" 
-  id="campo-pesquisa" placeholder="Digite o código do produto" aria-label="Search" aria-describedby="search-addon" />
-  <span class="input-group-text border-0" id="search-addon">
+<form class="buscar-cliente"  method="GET">
+  <div>
+    <input type="search" name="busca" class="form-control" />
+  </div>
+  <button type="submit" class="btn btn-dark">
     <i class="fas fa-search"></i>
-  </span>
-</div>
+  </button>
+</form>
+<a href="cliente.php">
+  <p class="limpar-busca">limpar busca</p>
+</a>
 
-<table class="table table-striped">
+
+<table class="table table-striped busca">
   <thead>
     <tr>
-      <th scope="col">Nome</th>
+      <th scope="col">ID</th>  
+      <th scope="col">Nome</th>  
       <th scope="col">CPF</th>
-      <th scope="col">email</th>
+      <th scope="col">Telefone</th>
+      <th scope="col">E-mail</th>
+      <th></th>
     </tr>
   </thead>
+
   <tbody>
-    <tr>
-      <td>Elias</td>
-      <td>22222222222</td>
-      <td>elias@gmail</td>
-    </tr>
-    <tr>
-      <td>Elias</td>
-      <td>22222222222</td>
-      <td>elias@gmail</td>
-    </tr>
-    <tr>
-      <td>Elias</td>
-      <td>22222222222</td>
-      <td>elias@gmail</td>
-    </tr>
-    <tr>
-      <td>Elias</td>
-      <td>22222222222</td>
-      <td>elias@gmail</td>
-    </tr>
-    <tr>
-      <td>Elias</td>
-      <td>22222222222</td>
-      <td>elias@gmail</td>
-    </tr>
-    <tr>
-      <td>Elias</td>
-      <td>22222222222</td>
-      <td>elias@gmail</td>
-    </tr>
+  <?php
+
+    include "../banco/config.php";
+
+    if (!empty($_GET)){
+      $pesquisa = $_GET['busca'];
+      if(is_numeric($pesquisa)) { // não dá erro com string
+        $busca = "SELECT * FROM cadastro_cliente WHERE nome = $pesquisa";
+        $query = $mysqli->query($busca);
+        $dados = $query->fetch_array();
+        if(!empty($dados)) { // não dá erro se não envontrar
+      ?>
+        <form action="addCarrinho.php" method="GET">
+            <tr>
+              <input type="hidden" name="codigoBarra" value="<?php echo $dados['codigoBarra']?>">
+              <td>
+                <?php echo $dados['codigoBarra'] ?>
+              </td>  
+              <td><?php echo $dados['marca']?></td>
+              <td>R$<?php echo $dados['valor']?></td>
+              <td>
+                <input type="number" id="quantidadeCliente" name="quantidadeCliente" min="1" max="<?php echo $dados['quantidadeEstoque'] ?>">
+              </td>
+              <td>
+                <button>comprar</button>
+              </td>
+            </tr>
+      <?php }}} ?>
+      </form>
   </tbody>
 </table>
 
